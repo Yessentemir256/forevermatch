b92b748cd9d58a2ce016219a02594d28360ed65f
Yessentemir <yessentemir.utepov@gmail.com>
2024-11-22 23:57:20 +0500

    server

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Forever Match!"))
+	})
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "pong",
+		})
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
